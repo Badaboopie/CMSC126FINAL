7ba0f4807c900ab90ef5da12a0fb56abf1bc6ae9
Eldon_ <130384980+El-dawn@users.noreply.github.com>
2024-05-28 18:14:34 +0800

    ignore

--- update.php
+++ update.php
@@ -1,5 +1,31 @@
 <?php
     include 'dbconnector.php';
 
-    switch ($_POST[''])
+    switch ($_POST['loctype']){
+        case 'Restaurant':
+            $loctype = $_POST['loctype'];
+            $locname = $_POST['locname'];
+            $address = $_POST['address'];
+            $owner = $_SESSION['userID'];
+            $contact = $_POST['contactno'];
+            break;
+        case 'Boarding House':
+            break;
+        case 'General Store':
+            break;
+        default:
+
+    }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
