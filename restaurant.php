d7816c786b6d8e787712f898c8f6ec7e1ac4a31f
Eldon_ <130384980+El-dawn@users.noreply.github.com>
2024-05-28 16:26:16 +0800

    ignored

---
+++ restaurant.php
@@ -0,0 +1,29 @@
+<?php
+    include 'dbconnector.php';
+
+    $sql = "SELECT * FROM establishmentLocation WHERE loc_type = 'restaurant' INNER JOIN restaurant ON establishmentLocation.loc_ID = restaurant.loc_ID";
+    $result = $conn->query($sql);
+    echo "<table>".
+        "<tr>".
+            "<th>Location ID: </th>".
+            "<th>Location Type: </th>".
+            "<th>Location Name: </th>".
+            "<th>Location Address: </th>".
+            "<th>Contact Number: </th>".
+            "<th>Restaurant Name: </th>".
+            "<th>Menu: </th>".
+        "</tr>";
+        while($row = $result->fetch_assoc()) {
+            echo "<tr>".
+                    "<td>".$row['loc_ID']."</td>".
+                    "<td>".$row['loc_type']."</td>".
+                    "<td>".$row['loc_name']."</td>".
+                    "<td>".$row['loc_address']."</td>".
+                    "<td>".$row['contact_no']."</td>".
+                    "<td>".$row['resto_name']."</td>".
+                    "<td>".$row['menu']."</td>".
+                "</tr>";
+        }
+    echo "</table>";
+    $conn->close();
+?>
